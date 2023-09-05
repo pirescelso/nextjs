1bd445a3d2c41a471600f9ad1d9707fcba590ef2
pirescelso <83796744+pirescelso@users.noreply.github.com>
2023-09-05 12:15:00 +0000

    Test of Result

--- bundesliga/src/backend/result/infra/db/prisma/result-prisma.repository.spec.ts
+++ bundesliga/src/backend/result/infra/db/prisma/result-prisma.repository.spec.ts
@@ -1,19 +1,117 @@
+import { prisma } from "@/backend/@prisma/prisma";
+import { initializePrisma } from "@/backend/@seedwork/tests/initialize-prisma";
 import { ResultPrismaRepository } from "./result-prisma.repository";
 
 describe("ResultPrismaRepository Unit Test", () => {
+  beforeEach(async () => {
+    await initializePrisma();
+    await prisma.$transaction([
+      prisma.ligaModel.create({
+        data: {
+          id: "1",
+          name: "Bundesliga 2023/24",
+        },
+      }),
+      prisma.teamModel.createMany({
+        data: [
+          {
+            id: "1",
+            name: "Team 1",
+          },
+          {
+            id: "2",
+            name: "Team 2",
+          },
+          {
+            id: "3",
+            name: "Team 3",
+          },
+          {
+            id: "4",
+            name: "Team 4",
+          },
+        ],
+      }),
+      prisma.gameDayModel.create({
+        data: {
+          id: "1",
+          round: 1,
+          ligaId: "1",
+        },
+      }),
+      prisma.gameModel.createMany({
+        data: [
+          {
+            id: "1",
+            gameDaylId: "1",
+            gameNumber: 1,
+            homeId: "1",
+            awayId: "2",
+          },
+          {
+            id: "2",
+            gameDaylId: "1",
+            gameNumber: 2,
+            homeId: "3",
+            awayId: "4",
+          },
+        ],
+      }),
+    ]);
+  }, 10000);
+
   it("should find a Result", async () => {
+    await prisma.$transaction([
+      prisma.resultModel.create({
+        data: {
+          id: "1",
+          gameDayId: "1",
+        },
+      }),
+      prisma.resultScoreModel.create({
+        data: {
+          id: "1",
+          resultId: "1",
+          gameId: "1",
+          homeGols: 1,
+          awayGols: 2,
+        },
+      }),
+    ]);
+
     const repository = new ResultPrismaRepository();
-    const result = await repository.findById("0");
-    expect(result.id).toBe("0");
-    expect(result.gameDayId).toBe("0");
-    expect(result.scores.length).toBe(2);
+    const result = await repository.findById("1");
+    expect(result.id).toBe("1");
+    expect(result.gameDayId).toBe("1");
+    expect(result.resultScores.length).toBe(1);
+    expect(result.resultScores[0].homeGols).toBe(1);
+    expect(result.resultScores[0].awayGols).toBe(2);
   });
 
   it("should return Result", async () => {
+    await prisma.$transaction([
+      prisma.resultModel.create({
+        data: {
+          id: "1",
+          gameDayId: "1",
+        },
+      }),
+      prisma.resultScoreModel.create({
+        data: {
+          id: "1",
+          resultId: "1",
+          gameId: "1",
+          homeGols: 1,
+          awayGols: 2,
+        },
+      }),
+    ]);
+
     const repository = new ResultPrismaRepository();
-    const result = await repository.search({ gameDayId: "0" });
-    expect(result.id).toBe("0");
-    expect(result.gameDayId).toBe("0");
-    expect(result.scores.length).toBe(2);
+    const result = await repository.search({ gameDayId: "1" });
+    expect(result.id).toBe("1");
+    expect(result.gameDayId).toBe("1");
+    expect(result.resultScores[0].homeGols).toBe(1);
+    expect(result.resultScores[0].awayGols).toBe(2);
   });
 });
